ed51e0f4102c49a009f7cd809d0b47c45bb323af
侯金博 <houjinbo@jlpay.com>
2020-11-02 10:42:46 +0800

    init

--- app/src/androidTest/java/com/jinbo/logan/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/jinbo/logan/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package com.jinbo.logan;
 
 import android.content.Context;
+
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
--- app/src/main/java/com/jinbo/logan/MainActivity.java
+++ app/src/main/java/com/jinbo/logan/MainActivity.java
@@ -1,6 +1,7 @@
 package com.jinbo.logan;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
